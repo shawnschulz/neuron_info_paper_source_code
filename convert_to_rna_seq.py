e764796eed14745ed6b2c1543f7d5503b88437c1
Shawn <shawnschulz@berkeley.edu>
2023-11-22 12:30:04 -0800

    convert_to_rna_seq now uses args, randomly samples (but have to manually change part that samples)

--- convert_to_rna_seq.py
+++ convert_to_rna_seq.py
@@ -6,16 +6,29 @@ from queue import Queue
 from threading import Thread
 from time import time
 import logging
+import random
+from optparse import OptionParser
+
+parser = OptionParser()
+
+parser.add_option("-a", "--adata_name", dest="adata_name")
+parser.add_option("-d", "--data_dir", dest="data_dir")
+parser.add_option("-s", "--super_cluster", dest="super_cluster")
+
+(options, args) = parser.parse_args()
+
+data_dir = options.data_dir
+adata_name = options.adata_name
+super_cluster = options.super_cluster
+
 #rationale behind threading over multiprocessing is that this is an IO bound task
 #want to save on memory over CPU power
 #(this is actually concurrency vs parallel tasks lol)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-os.chdir("../data")
-
-os.getcwd()
-
-
+os.chdir(data_dir)
+adata = sc.read_h5ad(adata_name)
+super_cluster_term = super_cluster 
 
 def multiply_lines_with_substring(multi_line_string, substring, n):
     #this is waaaaay too slow
@@ -33,43 +46,38 @@ def multiply_lines_with_substring(multi_line_string, substring, n):
 def utf8len(s):
     return len(s.encode('utf-8'))
 
-adata = sc.read_h5ad("sc_splatter.h5ad")
     #testing on just 10 cells for now
-adata = adata[adata.obs_names[0:500]]
+
+obs_names = list(adata.obs_names)
+random.shuffle(obs_names)
+adata = adata[obs_names[0:500]]
 
 ref_fasta_path = "ensembl_annotated_transcriptome.fa"
 
-super_cluster_term = "splatter_500"
 dir_name = super_cluster_term + "_fastas"
 subprocess.call(["mkdir", "-p", dir_name])
 
-out_fasta_fn =  super_cluster_term + '.fa' 
 
 string_to_write = "" #this string is gonna be REALLY long...
 
 cell_iteration_count=0
 gene_index_chosen_count=0
 
-subprocess.run(["echo", "", ">", out_fasta_fn])
+#subprocess.run(["echo", "", ">", out_fasta_fn])
 
 def append_expressed_transcripts_for_cell(cell_index):
-    print("The current cell index is {}".format(cell_index))
     cell_name = adata.obs_names[cell_index]
 
+    out_fasta_fn =  super_cluster_term + "_" + cell_name + '.fa' 
     rg_args = ""
     for gene_index in range(0, len(adata.var_names)):
-        print("The current gene_index is {}".format(gene_index))
         read_count = int(adata.X[cell_index, gene_index])
         ensembl_id = "gene:" + adata.var_names[gene_index]
         #should cross check the p-value of expression of the gene, hopefully it's already in the adata
         #we can use that to figure out how likely the gene is to actually be differentially expressed 
         if read_count > 0:
-            print("DEBUG: gene index chosen count is: {}".format(gene_index_chosen_count))
             rg_args = rg_args + ensembl_id + '\n'  
-        else:
-            print("no reads associated with this transcript")
 
-    print("DEBUG: printing rg_args: {}".format(rg_args))
     rg_args_fn = cell_name + "_temp_rg_args.txt"
     with open(rg_args_fn, "w") as file:
         file.write(rg_args)
@@ -114,13 +122,13 @@ ts = time()
 
 queue = Queue()
 
-for thread in range(8):
+#SHOVE MORE THREADS IN
+for thread in range(14):
     worker = transcript_worker(queue)
     worker.daemon = True
     worker.start()
 
 for cell_index in range(0, len(adata.obs_names)):
-    print(cell_index)
     queue.put(cell_index)
 
 queue.join()
